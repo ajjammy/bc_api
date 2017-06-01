3c520dde67d229468f2c9eb8d5cf62ce44bef6a7
liverbee <mis@nopadol.com>
2017-06-01 16:29:20 +0700

    Get Employee Compile

---
+++ bean/resp/employee.go
@@ -0,0 +1,31 @@
+package Resp
+
+import (
+	"github.com/jmoiron/sqlx"
+	"fmt"
+)
+
+type Employee struct {
+	Code string `json:"sale_code" db:"code"`
+	Name string `json:"sale_name" db:"name"`
+	Telephone string `json:"sale_telephone" db:"telephone"`
+	Profitcenter string `json:"profit_center" db:"profitcenter"`
+	ImageProfile string `json:"img_profile" db:"imageprofile"`
+}
+
+func(e *Employee)GetByKeyWord(keyword string,db *sqlx.DB)(ee []Employee,err error){
+	lcCommand := "select top 5 code,name,isnull(telephone,'') as telephone,'S01' as profitcenter,'' as imageprofile" +
+		" from bcnp.dbo.bcsale where code like '%"+keyword+"%' or name like '%"+keyword+"%'"
+	fmt.Println(lcCommand)
+	// Get saleorder from Database by docno
+	//cc = []Customer{}
+	err = db.Select(&ee,lcCommand)
+
+	if err !=nil{
+		return nil,err
+	}
+
+	fmt.Println(ee)
+
+	return ee,nil
+}
---
+++ ctrl/employee.go
@@ -0,0 +1,53 @@
+package ctrl
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	sale "github.com/satit13/bc_api/bean/resp"
+	api "github.com/satit13/bc_api/bean/resp"
+)
+
+
+func GetEmployee(c *gin.Context){
+	log.Println("call GET Employee")
+	c.Header("Server", "BC_API")
+	c.Header("Host", "nopadol.net:8001")
+	c.Header("Access-Control-Allow-Origin", "*")
+	//keyword := c.Param("keyword")
+	//token := c.Param("token")
+	//param1 := c.URL.Query().Get("param1")
+	access_token := c.Request.URL.Query().Get("access_token")
+	keyword := c.Request.URL.Query().Get("keyword")
+
+
+	fmt.Println("access_token = ",access_token)
+	emp := sale.Employee{}
+
+	//result := so.GetByDocno("test")
+	//fmt.Println("result object : ",result)
+	fmt.Println("call Employee.GetEmployee :",keyword)
+
+
+	ee,err := emp.GetByKeyWord(keyword,dbx)
+	if err != nil{
+		log.Println(err.Error())
+	}
+	fmt.Println(ee)
+
+	rs := api.Response{}
+
+	if err != nil {
+		rs.Status = "false"
+		rs.Message = "Search Employee error"
+		c.JSON(http.StatusNotFound, rs)
+	} else {
+		rs.Status = "true"
+		rs.Message = "Search Employee success"
+		rs.Data = ee
+		//rs.Link.Self = config.API_HOST + "/v1/users/"
+		c.JSON(http.StatusOK, rs)
+	}
+
+}
