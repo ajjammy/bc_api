a1a98a844ec8adddb6f548006098d6570de6cc6d
satit <satit@nopadol.com>
2017-06-12 15:15:31 +0700

    Update check isnull - saleorder.GetByDocno

--- bean/resp/saleorder.go
+++ bean/resp/saleorder.go
@@ -56,24 +56,81 @@ type Saleorder struct {
 
 type Saleordersub struct {
 	Id int64  `json:"id"`//roworder
-	Linenumber int64  `json:"line_number"`
+	//Linenumber int64  `json:"line_number"`
+	//Itemcode string `json:"item_code"`
+	//Itemname string `json:"item_name"`
+	//Qty float32`json:"qty"`
+	//Unitcode string `json:"unit_code"`
+	//Price  float32 `json:"price"`
+	//Amount float32 `json:"amounts"`
+	//Netamount float32 `json:"net_amount"`
+	//Packingrate1 float32 `json:"packing_rate_1"`
+	//Packingrate2 float32 `json:"packing_rate_2"`
+
+	Docno string `json:"docno"`
+	Taxtype int `json:"tax_type"`
 	Itemcode string `json:"item_code"`
+	Docdate string `json:"docdate" db:"docdate"`
+	Arcode string `json:"ar_code"`
+	Departcode string `json:"depart_code"`
+	Salecode string `json:"sale_code"`
+	Mydescription string `json:"my_description"`
 	Itemname string `json:"item_name"`
-	Qty float32`json:"qty"`
+	Whcode string `json:"wh_code"`
+	Shelfcode string `json:"shelf_code"`
+	Qty float32 `json:"qty"`
+	Remainqty float32 `json:"remain_qty"`
+	Price float32 `json:"price"`
+	Discountword string `json:"discount_word"`
+	Discountamount float32 `json:"discount amount"`
+	Amount float32 `json:"amount"`
+	Netamount float32 `json:"netamount"`
+	Homeamount float32 `json:"home_amount"`
 	Unitcode string `json:"unit_code"`
-	Price  float32 `json:"price"`
-	Amount float32 `json:"amounts"`
-	Netamount float32 `json:"net_amount"`
+	Iscancel int `json:"is_cancel"`
+	Linenumber int `json:"line_number"`
+	Categorycode string `json:"category_code"`
+	Groupcode string `json:"group_code"`
+	Brandcode string `json:"brand_code"`
+	Typecode string `json:"type_code"`
+	Formatcode string `json:"format_code"`
+	Barcode string `json:"barcode"`
+	Taxrate float32 `json:"tax_rate"`
 	Packingrate1 float32 `json:"packing_rate_1"`
 	Packingrate2 float32 `json:"packing_rate_2"`
+	
 }
 
 func(s *Saleorder)GetByDocno(docno string,db *sqlx.DB)(err error){
 
-	lcCommand := "select docno,docdate,billtype,departcode," +
-		"duedate,salecode,arcode,sumofitemamount,discountamount,beforetaxamount,taxamount,totalamount," +
-		"taxrate,isconfirm,billstatus,creatorcode,createdatetime,lasteditorcode,lasteditdatet,cancelcode,canceldatetime " +
-		" from bcnp.dbo.bcsaleorder where docno = '"+docno+"'"
+	lcCommand := "select docno,docdate,taxtype,billtype," +
+		"arcode,departcode,creditday,duedate,salecode,taxrate," +
+		"isconfirm," +
+		"isnull(mydescription,'') as mydescription ," +
+		"billstatus," +
+		"sostatus," +
+		"holdingstatus," +
+		"sumofitemamount," +
+		"isnull(discountword,'') as discountword," +
+		"isnull(discountamount,0) as discountamount," +
+		"isnull(afterdiscount,0)as afterdiscount," +
+		"isnull(beforetaxamount,0)as beforetaxamount," +
+		"isnull(taxamount,0) as taxamount," +
+		"isnull(totalamount,0) as totalamount," +
+		"isnull(netamount,0) as netamount," +
+		"isnull(iscancel,0)as iscancel," +
+		"isnull(creatorcode,'') as creatorcode," +
+		"isnull(createdatetime,'') as createdatetime," +
+		"isnull(lasteditorcode,'') as lasteditorcode," +
+		"isnull(lasteditdatet,'') as lasteditdatet," +
+		"isnull(confirmcode,'') as confirmcode," +
+		"isnull(confirmdatetime,'') as confirmdatetime," +
+		"isnull(cancelcode,'') as cancelcode," +
+		"isnull(canceldatetime,'') as canceldatetime," +
+		"isnull(isconditionsend,0) as isconditionsend," +
+		"isnull(deliveryday,0)as deliveryday," +
+		"isnull(deliverydate,getdate()) as deliverydate " +
+		"from bcnp.dbo.bcsaleorder where docno = '"+docno+"'"
 	fmt.Println(lcCommand)
 	// Get saleorder from Database by docno
 	//ss = []Saleorder{}
@@ -81,15 +138,39 @@ func(s *Saleorder)GetByDocno(docno string,db *sqlx.DB)(err error){
 	if err !=nil{
 		return err
 	}
-
-
 	fmt.Println(s)
 	// todo: add Node sub details
 
-	sosub := `select  a.roworder as id,a.linenumber,a.itemcode,a.itemname,a.qty
-	 		,a.unitcode,a.price,a.amount,a.netamount,a.packingrate1,a.packingrate2
-		from bcnp.dbo.bcsaleordersub a
-			where a.docno=? `
+	sosub := `select  roworder as id,
+			docno,taxtype,itemcode,docdate,arcode,
+			isnull(departcode,'') as departcode,
+			isnull(salecode,'') as salecode,
+			isnull(mydescription,'') as mydescription,
+			isnull(itemname,'') as itemname,
+			isnull(whcode,'') as whcode,
+			isnull(shelfcode,'')as shelfcode,
+			isnull(qty,0) as qty,
+			isnull(remainqty,0) as remainqty,
+			isnull(price,0) as price,
+			isnull(discountword,'') as discountword,
+			isnull(discountamount,0) as discountamount,
+			isnull(amount,0) as amount,
+			isnull(netamount,0) as netamount,
+			isnull(homeamount,0) as homeamount,
+			isnull(unitcode,'') as unitcode,
+			isnull(iscancel,0)as iscancel,
+			isnull(linenumber,0) as linenumber,
+			isnull(categorycode,'') as categorycode,
+			isnull(groupcode,'') as groupcode,
+			isnull(brandcode,'') as brandcode,
+			isnull(typecode,'') as typecode,
+			isnull(formatcode,'') as formatcode,
+			isnull(barcode,'') as barcode,
+			isnull(taxrate,0) as taxrate,
+			isnull(packingrate1,1) as packingrate1,
+			isnull(packingrate2,1) as packingrate2
+		from bcnp.dbo.bcsaleordersub
+			where docno=? `
 	fmt.Println(sosub)
 	err = db.Select(&s.Items,sosub,docno)
 	return err
@@ -108,7 +189,8 @@ func(s *Saleorder)GetByKeyWord(keyword string,db *sqlx.DB)(ss []Saleorder,err er
 	// todo : add child node in for loop
 	for i,so := range ss{
 		sosub := `select  a.roworder as id,a.linenumber,a.itemcode,a.itemname,a.qty
-	 		,a.unitcode,a.price,a.amount,a.netamount,a.packingrate1,a.packingrate2
+	 		,a.unitcode,a.price,a.amount,a.netamount,a.packingrate1,a.packingrate2,
+	 		a.docno,convert(char(10),a.docdate,103) as docdate,a.taxtype,a.arcode
 		from bcnp.dbo.bcsaleordersub a
 			where a.docno=? `
 		fmt.Println(sosub)
