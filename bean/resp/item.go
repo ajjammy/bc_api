bdb7f97dd1aa20616e8ec44c7abd380df78887ac
satit <satit@nopadol.com>
2017-06-24 14:11:17 +0700

    update api Endpoing : /customer + credit_day + id

--- bean/resp/item.go
+++ bean/resp/item.go
@@ -8,6 +8,7 @@ import (
 )
 
 type Item struct {
+	Id int64 `json:"id"`
 	Code string `json:"item_code"`
 	Name string `json:"item_name" db:"name1"`
 	UnitCode string `json:"unit_code" db:"defstkunitcode"`
@@ -52,7 +53,7 @@ type Unit struct {
 
 func(i *Item)GetByCode(itemcode string,db *sqlx.DB)(err error){
 
-	lcCommand := "select top 10 code,name1,defstkunitcode," +
+	lcCommand := "select top 10  roworder as Id, code,name1,defstkunitcode," +
 		"isnull(stockqty,0) as stockqty," +
 		"isnull(remainoutqty,0) as remainoutqty," +
 		"isnull(reserveqty,0) as reserveqty," +
@@ -86,7 +87,7 @@ func(i *Item)GetByCode(itemcode string,db *sqlx.DB)(err error){
 
 func(i *Item)GetByKeyword(keyword string,db *sqlx.DB)(items []Item,err error){
 
-	lcCommand := "select top 10  code,name1,defstkunitcode," +
+	lcCommand := "select top 10 roworder as Id, code,name1,defstkunitcode," +
 		"isnull(stockqty,0) as stockqty," +
 		"isnull(remainoutqty,0) as remainoutqty," +
 		"isnull(reserveqty,0) as reserveqty," +
