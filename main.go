db278b85e512ce852b667c4e12eafe0d08ddca00
satit <satit@nopadol.com>
2017-07-03 17:30:22 +0700

    (PUT) /quotation  - tested

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main(){
 
 	app.GET("/quotation",ctrl.GetQuotation)
 	app.POST("/quotation" , ctrl.PostNewQuotation)
-	//app.DELETE("/quotation" , ctrl.VoidQuotation)
+	app.DELETE("/quotation" , ctrl.VoidQuotation)
 	//app.PUT("/quotation", ctrl.PutQuotation)
 	//
 	//	app.get("/quotation", ctrl.GetQuotation)
