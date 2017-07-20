3c9f489882b81f148a53fec169da12ac5616cc80
satit13 <satit@nopadol.com>
2017-07-20 14:01:29 +0700

    add employee_test

--- bean/resp/employee_test.go
+++ bean/resp/employee_test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 )
 
+
+
 func TestEmployee_GetEmployeeCode(t *testing.T) {
 	e := Employee{}
 
@@ -12,5 +14,14 @@ func TestEmployee_GetEmployeeCode(t *testing.T) {
 	fmt.Println(em)
 	if em[0].Name != "สาธิต โฉมวัฒนา"{
 			t.Fatalf("Expected สาธิต  but got  : ",em[0].Name )
+	}
+}
+
+func TestEmployee_GetByKeyWord(t *testing.T) {
+	e := Employee{}
+	em,_ := e.GetByKeyWord("41054",dbtest)
+
+	if len(em)!=1 {
+		t.Fatalf("Exected 1 record but we got , ",len(em))
 	}
 }
